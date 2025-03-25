6a3661090f3a97f6e52bb896fa4cc747b894c90e
runixcronn <cemalettinreha92@gmail.com>
2025-03-25 16:24:57 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
   Routes,
   useNavigate,
 } from "react-router-dom";
-import ProtectedPage from "./pages/ProtectedPage";
+import ProtectedPage from "./pages/ProtectedPage.jsx";
 
 export default function App() {
   return (
